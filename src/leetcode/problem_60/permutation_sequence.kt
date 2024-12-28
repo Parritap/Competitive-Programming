2aae51dac48c6d6e8738e982577830b08d469b39
Parritap <juane.parrap@uqvirtual.edu.co>
2024-12-28 17:47:52 -0500

    Problem 60 : Permutation sequence DONE

--- src/leetcode/problem_60/permutation_sequence.kt
+++ src/leetcode/problem_60/permutation_sequence.kt
@@ -6,14 +6,18 @@ fun main() {
 
 
 fun getPermutation(n: Int, k: Int): String {
-    return calPermutations(n, k, "")
+    val list = IntArray(n) { it + 1 }
+    //list.forEach { print("$it ") }
+    return calPermutations(list, k - 1, "")
 }
 
-fun calPermutations(n: Int, k: Int, res: String): String {
-    if (n == 0) return res
-    val div = (n - 1).fact()
-    val pos = (k / div) + 1
-    return calPermutations(n - 1, k - (div * pos), res + pos.toString())
+fun calPermutations(list: IntArray, k: Int, res: String): String {
+    if (list.isEmpty()) return res
+    val div = (list.size - 1).fact()
+    val pos = k / div
+    val newList = list.filterIndexed { index, _ -> index != pos }.toIntArray()
+    val K = if (k == 0) 0 else k - (div * pos)
+    return calPermutations(newList, K, res + list[pos].toString())
 }
 
 fun Int.fact(): Int {
